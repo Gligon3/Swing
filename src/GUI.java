70f79f52ea09827888d4916c7ba5267e2af805c1
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2023-03-28 15:10:31 +0200

    La in det du visade för oss och gjorde inget mer än det

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class GUI {
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+public class GUI {
+    private JButton instructionsButton;
+    private JButton instructionsButton1;
+    private JButton instructionsButton2;
+    private JButton instructionsButton3;
+    private JButton timerButton;
+    private JRadioButton restartRadioButton;
+    private JRadioButton pressOpenRadioButton;
+    private JRadioButton setClockRadioButton;
+    private JButton startStopButton;
+    private JPanel Panel1;
+    private JTextArea textArea1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setContentPane(new GUI().Panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public GUI() {
+        String filename = "info.txt";
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            textArea1.setText("");
+            return;
+        }
+        String nextLine = null;
+        try {
+            nextLine = in.readLine();
+            while (nextLine != null) {
+                assert textArea1 != null;
+                textArea1.append(nextLine +"\n");
+                nextLine = in.readLine();
+            }
+        } catch (IOException ex) {
+            textArea1.setText("");
+        }
+
+
+        filename = "text.txt";
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,"Failed to Save!");
+        }
+        out.println("Hello World");
+        out.flush();
+        out.close();
+    }
 }
+
